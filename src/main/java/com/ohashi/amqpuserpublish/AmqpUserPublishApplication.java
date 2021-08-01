03f62da7b97580d11bcfcb15049fe3a099ecc88d
ImOhashi <leonardoohashi.mkt@gmail.com>
2021-08-01 14:32:03 -0300

    first commit

---
+++ src/main/java/com/ohashi/amqpuserpublish/AmqpUserPublishApplication.java
@@ -0,0 +1,13 @@
+package com.ohashi.amqpuserpublish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmqpUserPublishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmqpUserPublishApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ohashi/amqpuserpublish/AmqpUserPublishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohashi.amqpuserpublish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmqpUserPublishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
